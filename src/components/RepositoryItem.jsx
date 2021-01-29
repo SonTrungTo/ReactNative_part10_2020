5f28fe9cbb316ef99de57b53c196a1767a81ad2e
SonTrungTo <son.trung.to@gmail.com>
2021-01-30 01:56:40 +0200

    Still refactoring codes, 10.5, nearly finished!

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,40 +1,18 @@
 import React from "react";
-import { View, StyleSheet, Image } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 import theme from "./theme";
 import Text from "./Text";
+import Description from "./RepositoryListItem/Description";
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: theme.colors.itemBackgroundColor,
         padding: 20
     },
-    basic: {
-        flexDirection: 'row',
-        marginBottom: 25
-    },
-    avatarSize: {
-        width: 60,
-        height: 60,
-        flexGrow: 0,
-        borderRadius: 5
-    },
-    description: {
-        flexGrow: 1,
-        marginLeft: 20,
-        flexShrink: 1
-    },
     status: {
         flexDirection: 'row',
         justifyContent: 'space-around'
-    },
-    badge: {
-        borderStyle: 'solid',
-        backgroundColor: theme.colors.primary,
-        borderRadius: 5,
-        textAlign: 'center',
-        padding: 5,
-        flexGrow: 0
     }
 });
 
@@ -44,21 +22,12 @@ stargazersCount, ratingAverage, reviewCount,
 ownerAvatarUrl }) => {
     return (
         <View style={ styles.container }>
-            <View style={styles.basic}>
-                <Image 
-                style={styles.avatarSize}
-                source={{
-                    uri: ownerAvatarUrl
-                }}
-                />
-                <View style={styles.description}>
-                    <Text fontWeight="bold" fontSize="subheading">{fullName}</Text>
-                    <Text color="textSecondary" style={{marginTop: 5}}>{description}</Text>
-                    <View style={{ flexDirection: 'row', marginTop: 15 }}>
-                        <Text color="textBar" style={styles.badge}>{language}</Text>
-                    </View>
-                </View>
-            </View>
+            <Description
+                fullName={fullName}
+                description={description}
+                language={language}
+                ownerAvatarUrl={ownerAvatarUrl}
+            />
             <View style={styles.status}>
                 <View style={{ flexDirection: 'column' }}>
                     <Text fontWeight="bold" fontSize="subheading">{forksCount >= 1000 ? Number(forksCount / 1000).toFixed(1) + 'k' : forksCount}</Text>
---
+++ src/components/RepositoryListItem/Description.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+import Avatar from "./Avatar";
+import Text from "../Text";
+import theme from "../theme";
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        marginBottom: 25
+    },
+    content: {
+        flexGrow: 1,
+        marginLeft: 20,
+        flexShrink: 1
+    },
+    description: {
+        marginTop: 5
+    },
+    language: {
+        flexDirection: 'row',
+        marginTop: 15
+    },
+    badge: {
+        borderStyle: 'solid',
+        backgroundColor: theme.colors.primary,
+        borderRadius: 5,
+        textAlign: 'center',
+        padding: 5,
+        flexGrow: 0
+    }
+});
+
+const Description = ({ ownerAvatarUrl, 
+fullName, description, language }) => {
+    return (
+        <View style={styles.container}>
+            <Avatar ownerAvatarUrl={ownerAvatarUrl} />
+            <View style={styles.content}>
+                <Text fontWeight="bold"
+                fontSize="subheading">{fullName}</Text>
+                <Text color="textSecondary"
+                style={styles.description}>{description}</Text>
+                <View style={styles.language}>
+                    <Text color="textBar"
+                    style={styles.badge}>{language}</Text>
+                </View>
+            </View>
+        </View>
+    );
+};
+
+export default Description;
