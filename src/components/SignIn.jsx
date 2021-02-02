4b5b4ba81bc98ed2300fef42bc9aef7c91f35321
SonTrungTo <son.trung.to@gmail.com>
2021-02-02 17:33:10 +0200

    still solving 10.8

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
+
+import { Formik } from "formik";
 
 import Text from "./Text";
+import FormikTextInput from "./FormikTextInput";
+
+const SignInForm = ({ onSubmit }) => {
+    return (
+        <View>
+            <FormikTextInput />
+        </View>
+    );
+};
 
 const SignIn = () => {
     return (
