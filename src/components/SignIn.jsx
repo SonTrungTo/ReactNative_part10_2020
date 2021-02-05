eeed31d4dc45ad0768f93b15b869eb2a1bcd8c09
SonTrungTo <son.trung.to@gmail.com>
2021-02-05 22:56:27 +0200

    10.8 solved!

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,22 +1,62 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, TouchableWithoutFeedback,
+    StyleSheet } from "react-native";
 
 import { Formik } from "formik";
 
 import Text from "./Text";
 import FormikTextInput from "./FormikTextInput";
+import theme from "./theme";
+
+const initialValues = {
+    username: '',
+    password: ''
+};
+
+const styles = StyleSheet.create({
+    submitTouch: {
+        backgroundColor: theme.colors.primary,
+        textAlign: 'center',
+        padding: 15,
+        margin: 10,
+        borderRadius: 5
+    },
+    container: {
+        backgroundColor: theme.colors.textBar
+    }
+});
 
 const SignInForm = ({ onSubmit }) => {
     return (
-        <View>
-            <FormikTextInput />
+        <View style={styles.container}>
+            <FormikTextInput name="username" placeholder="Username" />
+            <FormikTextInput name="password" placeholder="Password"
+            secureTextEntry />
+            <TouchableWithoutFeedback onPress={onSubmit}>
+                <Text
+                color="textBar"
+                fontWeight="bold"
+                style={styles.submitTouch}>
+                    Sign In
+                </Text>
+            </TouchableWithoutFeedback>
         </View>
     );
 };
 
 const SignIn = () => {
+    const onSubmit = (values) => {
+        console.log(values);
+    };
+
     return (
-        <Text>The sign in View</Text>
+        <Formik initialValues={initialValues} onSubmit={onSubmit}>
+            {({ handleSubmit }) => {
+                return (
+                    <SignInForm onSubmit={ handleSubmit } />
+                );
+            } }
+        </Formik>
     );
 };
 
--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { StyleSheet, TextInput as NativeTextInput } from "react-native";
 
+import theme from "./theme";
+
 const styles = StyleSheet.create({
-    container: {}
+    container: {
+        backgroundColor: theme.colors.textBar,
+        padding: 15,
+        margin: 10,
+        borderRadius: 5,
+        borderStyle: 'solid',
+        borderWidth: 1
+    }
 });
 
 const TextInput = ({ style, ...props }) => {
