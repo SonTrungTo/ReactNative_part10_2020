b56dc44840122b67ccdc58f62d41e9ab8ba8f24d
SonTrungTo <son.trung.to@gmail.com>
2021-02-07 22:58:20 +0200

    preparing for 10.12, env variables

---
+++ app.config.js
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+
+export default {
+  name: "ReactNative_part10",
+  slug: "ReactNative_part10",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff"
+  },
+  updates: {
+    fallbackToCacheTimeout: 0
+  },
+  assetBundlePatterns: [
+    "**/*"
+  ],
+  ios: {
+    supportsTablet: true
+  },
+  web: {
+    favicon: "./assets/favicon.png"
+  },
+  extra: {
+    env: process.env.ENV
+  }
+};
