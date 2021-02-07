3549f11a262d354de391c51646f133845bb59810
SonTrungTo <son.trung.to@gmail.com>
2021-02-07 17:23:23 +0200

    useRepostories hooks now fetch real data from backend

--- src/components/AppBarTab.jsx
+++ src/components/AppBarTab.jsx
@@ -1,8 +1,18 @@
 import React from "react";
-import { TouchableWithoutFeedback } from "react-native";
+import { TouchableWithoutFeedback, StyleSheet } from "react-native";
 import { Link } from "react-router-native";
 
 import Text from "./Text";
+import theme from "./theme";
+
+const styles = StyleSheet.create({
+    appBarTab: {
+        padding: 15,
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: theme.colors.primary
+    }
+});
 
 
 const AppBarTab = ({ name, to }) => {
@@ -10,7 +20,8 @@ const AppBarTab = ({ name, to }) => {
         <TouchableWithoutFeedback>
             <Link to={to || '/'}>
                 <Text fontWeight="bold"
-                color="textBar">
+                color="textBar"
+                style={styles.appBarTab}>
                     {name}
                 </Text>
             </Link>
