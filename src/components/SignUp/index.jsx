ff5cd0cff69a8c3178135cc4cf02a86f43af6efc
SonTrungTo <son.trung.to@gmail.com>
2021-02-20 07:51:52 +0200

    10.22 in progress...

---
+++ src/components/SignUp/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, TouchableOpacity,
+    StyleSheet } from "react-native";
+import { Formik } from "formik";
+import * as yup from "yup";
+
+import FormikTextInput from "../FormikTextInput";
+
+const SignUpForm = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+};
+
+const SignUp = () => {
+
+};
+
+export default SignUp;
