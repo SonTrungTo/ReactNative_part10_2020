dd81545dda7e813f3ac21819bb831dbfdd185def
SonTrungTo <son.trung.to@gmail.com>
2021-02-20 08:24:56 +0200

    10.22 writing form code...

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -5,11 +5,39 @@ import { Formik } from "formik";
 import * as yup from "yup";
 
 import FormikTextInput from "../FormikTextInput";
+import Text from "../Text";
 
-const SignUpForm = () => {
+const styles = StyleSheet.create({
+    signupButton: {
+        
+    },
+    container: {
+
+    }
+});
+
+const inititalValues = {
+    username: '',
+    password: '',
+    passwordConfirm: ''
+};
+
+const SignUpForm = ({ onSubmit }) => {
     return (
         <View>
-            
+            <FormikTextInput name="username"
+            placeholder="Username" />
+            <FormikTextInput name="password"
+            placeholder="Password" />
+            <FormikTextInput name="passwordConfirm"
+            placeholder="Password confirmation" />
+            <TouchableOpacity onPress={onSubmit}>
+                <Text fontWeight="bold"
+                color="textBar"
+                style={}>
+                    Sign Up
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 };
